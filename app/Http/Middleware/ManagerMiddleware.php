63345ebb2ce17f8f06615c902f57fda6a51ecede
Nessa <na.nadine.001@gmail.com>
2023-11-23 15:48:13 +0700

    add middleware for each role_id

--- app/Http/Middleware/ManagerMiddleware.php
+++ app/Http/Middleware/ManagerMiddleware.php
@@ -6,12 +6,12 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class OperatorMiddleware
+class ManagerMiddleware
 {
     public function handle($request, Closure $next)
     {
         // Pemeriksaan role_id untuk operator
-        if ($request->user() && $request->user()->role_id >= 50) {
+        if ($request->user() && $request->user()->role_id >= 90) {
             return $next($request);
         }
 
--- app/Http/Middleware/SuperadminMiddleware.php
+++ app/Http/Middleware/SuperadminMiddleware.php
@@ -6,12 +6,12 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class OperatorMiddleware
+class SuperadminMiddleware
 {
     public function handle($request, Closure $next)
     {
         // Pemeriksaan role_id untuk operator
-        if ($request->user() && $request->user()->role_id >= 50) {
+        if ($request->user() && $request->user()->role_id >= 99) {
             return $next($request);
         }
 
